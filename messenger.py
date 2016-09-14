f9e0a4a1650f95b76c7f87e3406ad5c9fb1dc719
Franco Sarachu <fsarachu@gmail.com>
2016-09-13 23:20:19 -0300

    Successfully sent a message with this code!

--- messenger.py
+++ messenger.py
@@ -1,3 +1,12 @@
-import twilio
+from twilio.rest import TwilioRestClient
 
-print twilio.__version__
+account_sid = "ACCOUNT_SID"  # Your Account SID from www.twilio.com/console
+auth_token = "AUTH_TOKEN"  # Your Auth Token from www.twilio.com/console
+
+client = TwilioRestClient(account_sid, auth_token)
+
+message = client.messages.create(body="Alo alo! Franco is messing up with Twilio!",
+                                 to="+59812345678",  # Replace with your phone number
+                                 from_="+19012345678")  # Replace with your Twilio number
+
+print(message.sid)
